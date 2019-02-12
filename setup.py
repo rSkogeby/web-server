7cbc413c7834ff6044faed26822044b88feef7ca
Richard Skogeby <richard.skogeby@outlook.com>
2019-02-12 20:03:18 +0000

    Add response

--- setup.py
+++ setup.py
@@ -5,9 +5,17 @@ class webserverHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
             if self.path.endswith('\hello'):
-                pass
-        except KeyboardInterrupt as e:
-            print('Error: ', e)
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ''
+                output += '<html><body>Hello!</body></html>'
+                self.wfile.write(output)
+                print(output)
+                return
+        except IOError:
+            self.send_error(404, 'File Not Found %s', self.path)
 
 
 def main():
