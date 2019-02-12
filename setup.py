eb7306b2a752bf10174174e669cffa9f50b1a64f
Richard Skogeby <richard.skogeby@outlook.com>
2019-02-12 19:58:11 +0000

    Add webserverHandler class

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
+
+class webserverHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path.endswith('\hello'):
+                pass
+        except KeyboardInterrupt as e:
+            print('Error: ',)
+
+
 def main():
-    pass
+    port = 8080
+    server = HTTPServer(('', port), webserverHandler)
+    server.serve_forever()
 
 if __name__ == "__main__":
     main()
